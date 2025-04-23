a251bf8f6084839f29ec3785330460ecb4990543
Cornel-MIT <www.ccornel@gmail.com>
2025-04-23 23:38:24 +0200

    Added User form Validations

--- src/app/template-driven-form/template-driven-form.component.ts
+++ src/app/template-driven-form/template-driven-form.component.ts
@@ -1,24 +1,64 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
 import { User } from '../user';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-template-driven-form',
-  imports: [FormsModule],
+  imports: [FormsModule, JsonPipe],
   templateUrl: './template-driven-form.component.html',
   styleUrl: './template-driven-form.component.scss'
 })
-export class TemplateDrivenFormComponent {
-
+export class TemplateDrivenFormComponent implements OnInit{
+  
   userObject : User = {
     firsName: '',
     lastName: '',
     emailAddress: '',
     password: undefined
   };
-
+  
   onSubmit(userForm:NgForm) {
     console.log(userForm.value);
     
   } 
+  
+  ngOnInit(): void {
+    // this.userObject = {
+      //   firstName: 'Joe',
+      //   lastName: 'Goldburg',
+      //   emailAddress: 'joe@gmail.com',
+      //   password: 12345, 
+      //   isCheck: true
+      // }
+    }
+    
+    
+    setValues(userForm: NgForm) {
+      let obj = {
+        firstName: 'Joe',
+        lastName: 'Goldburg',
+        emailAddress: 'joe@gmail.com',
+        password: 12345, 
+        isCheck: true
+      }
+      
+      userForm.setValue(obj)
+    }
+
+    patchValues(userForm: NgForm) {
+         let obj = {
+           firstName: 'Joe',
+           lastName: 'Goldburg',
+          emailAddress: 'joe@gmail.com',
+        }
+        
+        userForm.control.patchValue(obj)
+    }
+
+     resetValues(userForm: NgForm) {
+      //  userForm.reset();
+       userForm.resetForm();
+     }
+
 }
