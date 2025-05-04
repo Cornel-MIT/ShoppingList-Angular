6e693bc41aec6de21278cf6a00e11f2245fe5483
Cornel-MIT <www.ccornel@gmail.com>
2025-05-04 15:23:02 +0200

    Added Dummy users

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,20 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SharedDataService } from '../shared-data.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-profile',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
 
   dummyData : any;
   isEligible : boolean;
+  apiData : any;
+
   constructor(private _sharedData : SharedDataService) {
       this.dummyData = this._sharedData.userData;
 
       this.isEligible = this._sharedData.isEligibleForSubscription()
+  }
+
+  ngOnInit(): void {
+      this.getData();
+  }
+
+  getData() {
+    this._sharedData.getUserData().subscribe(res=> {
+        this.apiData = res;
+    })
   }
     
   //  userData = {
--- src/app/shared-data.service.ts
+++ src/app/shared-data.service.ts
@@ -1,11 +1,18 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SharedDataService {
+
+  API_URL = "https://jsonplaceholder.typicode.com/users"
   
-  constructor() { }
+  constructor(private _http : HttpClient) { }
+
+  getUserData( ) {
+    return this._http.get(this.API_URL)
+  }
 
   userData = {
     id : 1,
