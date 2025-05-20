168575577d6877540c14f56de1498136530313b4
Cornel-MIT <www.ccornel@gmail.com>
2025-05-20 23:04:48 +0200

    Created cookies to collect browser data

--- src/app/storage/storage.component.ts
+++ src/app/storage/storage.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-storage',
@@ -7,21 +8,40 @@ import { Component } from '@angular/core';
   styleUrl: './storage.component.scss'
 })
 export class StorageComponent {
+  
+  constructor(private cookie : CookieService) {}
 
   sessionValue : string = "";
+
+  cookieValue : any
   
   
   setSession() {
     sessionStorage.setItem('name', 'session')
     sessionStorage.setItem('pass', 'session123')
   }
-
+  
   getSession() {
    this.sessionValue = sessionStorage.getItem('name') ?? "";
   }
-
+  
   removeSession() {
     sessionStorage.removeItem('pass')
+  }
+  
+
+  setCookie() {
+    this.cookie.set('token1', '12345', 1);
+    this.cookie.set('token2', 'xyz');
+  }
+  
+  getCookie() {
+   this.cookieValue = this.cookie.get('token1');
+  }
+
+  deleteCookie() {
+     this.cookie.delete('token1');
+     this.cookieValue = "";
   }
 
   clearSession() {
